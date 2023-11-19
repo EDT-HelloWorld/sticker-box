fca26881a7dceab53abc1192a47efc4d7a3e6cae
letsjo <hyunoh.jo@gmail.com>
2023-11-20 00:10:59 +0900

    Feat: add comment for method

--- src/utils/getRandomColor.js
+++ src/utils/getRandomColor.js
@@ -1,3 +1,7 @@
+/**
+ * @description 랜덤한 색상을 반환해주는 함수
+ * @param {number} alpha
+ */
 export function getRandomColor(alpha = 1) {
   const randomNumbers = Array.from({ length: 3 }, () => Math.floor(Math.random() * 50) + 150);
   return `rgba(${randomNumbers[0]}, ${randomNumbers[1]}, ${randomNumbers[2]}, ${alpha})`;
